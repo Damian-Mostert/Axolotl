d462b5f1dc5596674394fd942eb923f3097f0202
Damian <damian@fusebox.co.za>
2025-11-26 16:22:36 +0200

    removed unneeded files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/damian/Axolotl
-# Build directory: /Users/damian/Axolotl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
